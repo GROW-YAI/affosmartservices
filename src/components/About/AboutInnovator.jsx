9dfd134d83af5ee700a30adfb4f5bd5f394767b0
menniia <mennia17@gmail.com>
2024-12-12 23:16:18 +0000

    fixed about innovator margin

--- src/components/About/AboutInnovator.jsx
+++ src/components/About/AboutInnovator.jsx
@@ -30,23 +30,23 @@ const AboutInnovator = () => {
                             animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
                             transition={{ duration: 0.7 }}
                         >
-                            <h3 className="text-2xl font-semibold">Samuel Affo</h3>
-                            <p className="text-gray-600">
+                            <h3 className="text-3xl text-green-900 pt-6 ml-16 font-semibold">Samuel Affo</h3>
+                            <p className="text-gray-700 ml-14">
                                 Samuel Affo is a Ghanaian native whose roots in a farming community instilled in him a deep passion for
                                 agriculture, particularly vegetable farming.
                             </p>
-                            <p className="text-gray-600">
+                            <p className="text-gray-700 ml-14">
                                 He is the founder and the CEO of AFFOsmart Services; a startup company best known for selling and
                                 delivering high-quality foodstuffs to its customers at a competitive price. This came in as solace for
                                 farmers who suffer exploitation from middlemen and also to eliminate the stress consumers go through
                                 in getting their selves some foodstuffs; a very healthy and high-quality produce.
                             </p>
-                            <p className="text-gray-600">
+                            <p className="text-gray-700 ml-14">
                                 With a blend of agricultural expertise and artistic prowess, Samuel Affo embodies a unique blend of
                                 creativity, resilience, and compassion, poised to leave a lasting legacy through his advocacy for social
                                 change.
                             </p>
-                            <div className="flex space-x-4">
+                            <div className="flex space-x-4 ml-14">
                                 <span
                                     className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-green-100 text-green-800">
                                     10+ Years Experience in Farming
